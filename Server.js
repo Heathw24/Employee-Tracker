3fe4aea746b6be5439e2f76e0287f686f20c2ecf
Heath Ward <heathw24@outlook.com>
2021-01-24 22:22:53 -0500

    fixed display functions and worked on updating function

--- Server.js
+++ Server.js
@@ -67,3 +67,127 @@ function runSearch() {
             }
         });
 }
+
+//======= Add functions ==================================
+function addDepartment() {
+    inquirer
+      .prompt({
+          name: "newdepartment",
+          type: "input",
+          message: "What is the name of the new department?"
+      })
+      .then(function(answer) {
+          var query = "INSERT INTO department (depName) VALUES (?)";
+          connection.query(query, answer.newdepartment, function(err, res) {
+           if (err) throw err;
+           console.log("the " + answer.newdepartment + "department was created");
+           runSearch();
+          });
+      });
+}
+
+function addRoles() {
+    inquirer
+      .prompt({
+          name: "newdepartment",
+          type: "input",
+          message: "What is the name of the new department?"
+      })
+      .then(function(answer) {
+          var query = "INSERT INTO department (depName) VALUES (?)";
+          connection.query(query, answer.newdepartment, function(err, res) {
+           if (err) throw err;
+           console.log("the " + answer.newdepartment + "department was created");
+           runSearch();
+          });
+      });
+}
+
+//==== Display functions ==================================
+
+function viewDepartments(){
+    connection.query("SELECT * FROM department", function(err, res) {
+    if (err) throw err;
+    for (var i = 0; i < res.length; i++){
+        console.log(
+            "Department ID: " + res[i].id + " || Department Name: " + res[i].depName
+        );
+    }
+
+    runSearch();
+    });
+}
+
+function viewRoles(){
+    connection.query("SELECT * FROM roles", function(err, res) {
+    if (err) throw err;
+    for (var i = 0; i < res.length; i++){
+        console.log(
+            "Role ID: " + res[i].id + " || Title: " + res[i].title + " || Salary: " + res[i].salary + " || Department ID: " + res[i].department_id
+        );
+    }
+
+
+    runSearch();
+    });
+}
+
+function viewEmployees(){
+    connection.query("SELECT * FROM employee", function(err, res) {
+    if (err) throw err;
+    for (var i = 0; i < res.length; i++){
+        console.log(
+            "Employee ID: " + res[i].id + " || First Name: " + res[i].first_name + " || Last Name: " + res[i].last_name + " || Role ID: " + res[i].role_id + " || Manager ID: " + res[i].manager_id
+        );
+    }
+
+    runSearch();
+    });
+}
+
+//===== updating functions ====================
+
+function updateEmployee(){
+    inquirer
+    .prompt({
+        name: "employee",
+        type: "input",
+        message: "Which employee do you want to update?"
+    },
+    {
+        name: "role",
+        type: "input",
+        message: "What new role does this employee have?"
+    })
+    .then(function(answer) {
+
+        var employee;
+
+        connection.query("SELECT * FROM employee", function(err, res) {
+            if (err) throw err;
+            for (var i = 0; i < res.length; i++){
+                var name = res[i].first_name + " " + res[i].last_name;
+
+                if (name == answer.employee){
+                  employee = res[i];
+                  console.log(employee);
+                  return employee;
+                } 
+                console.log("this employee doesnt exist");
+            }
+        });
+
+        
+        connection.query("SELECT * FROM roles", function(err, res) {
+            if (err) throw err;
+            for (var i = 0; i < res.length; i++){
+               var role = 
+            }
+
+        });
+
+        
+      runSearch();
+    });
+}
+
